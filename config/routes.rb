446a712650b73aa75a760d20a6ae9502719697fa
Tim <timcarey1989@gmail.com>
2022-05-04 18:38:32 -0400

    fixed subreddit routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :subreddits, path: "/r"
   root "home#index"
+
+  resources :subreddits, only: %i[show], path: "/r"
+
+  resources :subreddits, only: %i[index edit create new update destroy]
+
+  get "/r", to: redirect('/subreddits')
 end
