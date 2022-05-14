7a6092ee8bcfbed037ace2c909360ad1dd53fb6b
Timothy Carey <timcarey1989@gmail.com>
2022-05-14 19:47:31 -0400

    removed user association to subs for now

--- app/models/subreddit.rb
+++ app/models/subreddit.rb
@@ -1,10 +1,6 @@
 class Subreddit < ApplicationRecord
-  validates :name, format: {with: /\A[a-zA-Z]+\Z/}, uniqueness: {case_sensitive: false}, presence: true
+  validates :name, format: {with: /\A[a-zA-Z]+\Z/}, uniqueness: {case_sensitive: true}, presence: true
   validates :sub_description, presence: true
-
-  validates_uniqueness_of :name, case_sensitive: true
-
-  belongs_to :user
 
   extend FriendlyId
   friendly_id :name, use: :slugged
